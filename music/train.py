614e54597016b65b42ca24244c614129f907e9ea
silverbackking <me@justinglibert.com>
2017-04-02 03:24:14 +0100

    g

--- music/train.py
+++ music/train.py
@@ -6,68 +6,67 @@ import glob
 from tqdm import tqdm
 import midi_manipulation
 from tensorflow.python.ops import control_flow_ops
-import pickle
 
 path="/home/ec2-user/ai/deeprap/music/blues"
 files=glob.glob('{}/*.*mid*'.format(path))
 songs=[]
-
-
 for f in tqdm(files):
             song = np.array(midi_manipulation.midiToNoteStateMatrix(f))
             if np.array(song).shape[0] > 50:
                 songs.append(song)
 
+#MusicParams
+
 lowestnote=midi_manipulation.lowerBound
 highestnote=midi_manipulation.upperBound
 noterange=highestnote-lowestnote
 
-#Hyperparams
-timesteps=50 #no of notes to create at a time
-nv=2*noterange*timesteps #visible layer size.
-nh=50 #hidden layer size.
-epochs=2000
-batch_size=100
-lr=tf.constant(0.005, tf.float32) #learning rate
 
+#Placeholders & vars for Network
 
 x=tf.placeholder(tf.float32, [None, nv], name="x") #The placeholder for data
 W=tf.Variable(tf.random_normal([nv, nh], 0.01), name="W")#weight matrix
 bh=tf.Variable(tf.zeros([1, nh],  tf.float32, name="bh")) #bias for hidden layer
 bv=tf.Variable(tf.zeros([1, nv],  tf.float32, name="bv")) #bias for visible layer
 
+
+
+
+#Sampling helpers
+
 def sample(probs):
     return tf.floor(probs+tf.random_uniform(tf.shape(probs), 0, 1))
 
 def gibbs_step(count, k, xk):
         #Runs a single gibbs step. The visible values are initialized to xk
-        hk = sample(tf.sigmoid(tf.matmul(xk, W) + bh))
-        xk = sample(tf.sigmoid(tf.matmul(hk, tf.transpose(W)) + bv))
+        hk=sample(tf.sigmoid(tf.matmul(xk, W) + bh))
+        xk=sample(tf.sigmoid(tf.matmul(hk, tf.transpose(W)) + bv))
         return count+1, k, xk
 
 def gibbs_sample(k):
     #Gibbs sample(done for k iterations) is used to approximate the distribution of the RBM(defined by W, bh, bv)
-    ct = tf.constant(0)
-    [_, _, x_sample] = control_flow_ops.while_loop(lambda count, num_iter, *args: count < num_iter,gibbs_step, [ct, tf.constant(k), x], parallel_iterations=1, back_prop=False)
+    ct=tf.constant(0)
+    [_, _, x_sample]=control_flow_ops.while_loop(lambda count, num_iter, *args: count < num_iter,gibbs_step, [ct, tf.constant(k), x], parallel_iterations=1, back_prop=False)
     #to stop tensorflow from propagating gradients back through the gibbs step
-    x_sample = tf.stop_gradient(x_sample)
+    x_sample=tf.stop_gradient(x_sample)
     return x_sample
 
 #backward pass, x samples drawn from prob distribution defn by (hk,w,bv)
-x_sample = gibbs_sample(1)
+x_sample=gibbs_sample(1)
 #h sampled from prob distrib defn by (x,w,bh)
-h = sample(tf.sigmoid(tf.matmul(x, W) + bh))
+h=sample(tf.sigmoid(tf.matmul(x, W) + bh))
 #h calculated from prob distrib defn by (x_sample,w,bh)
-h_sample = sample(tf.sigmoid(tf.matmul(x_sample, W) + bh))
+h_sample=sample(tf.sigmoid(tf.matmul(x_sample, W) + bh))
+
 
 #contrastive divergence algorithm
 #Update W, bh, and bv, based on the difference between the samples that are drawn and the original values
-size_tr = tf.cast(tf.shape(x)[0], tf.float32)
-eta = lr/size_tr
-W_upd = tf.multiply(eta, tf.subtract(tf.matmul(tf.transpose(x), h), tf.matmul(tf.transpose(x_sample), h_sample)))
-bv_upd = tf.multiply(eta, tf.reduce_sum(tf.subtract(x, x_sample), 0, True))
-bh_upd = tf.multiply(eta, tf.reduce_sum(tf.subtract(h, h_sample), 0, True))
-updt = [W.assign_add(W_upd), bv.assign_add(bv_upd), bh.assign_add(bh_upd)]
+size_tr=tf.cast(tf.shape(x)[0], tf.float32)
+eta=lr/size_tr
+W_upd=tf.multiply(eta, tf.subtract(tf.matmul(tf.transpose(x), h), tf.matmul(tf.transpose(x_sample), h_sample)))
+bv_upd=tf.multiply(eta, tf.reduce_sum(tf.subtract(x, x_sample), 0, True))
+bh_upd=tf.multiply(eta, tf.reduce_sum(tf.subtract(h, h_sample), 0, True))
+updt=[W.assign_add(W_upd), bv.assign_add(bv_upd), bh.assign_add(bh_upd)]
 
 
 sess = tf.Session()
